a4a607fcc9eee5c2e9aef7b272a1a495f387109b
Rishbh Sharma <rishbhsharma2008@gmail.com>
2020-09-23 04:53:41 +0530

    Update reference_design.py
    added more functionality

--- reference_design.py
+++ reference_design.py
@@ -1,29 +1,111 @@
 from random import randint
+from time import time,sleep
 
 #class DS:
 #    def __init__(self, data1, data2):
 #        self.d1=data1
 #        self.d2=data2
 
-class System():
+delay_ms = 500
+
+def wait(): sleep(delay_ms/1000.0)
+def wait_r(): sleep(delay_ms/1000.0*randint(1,10))
+
+'''
+class Status:
+    def __init__(self):
+        self.last_seen = time()
+        self.is_online = False
+    
+    def came_online(self):
+        self.is_online = True
+        self.last_seen = time()
+        
+    def went_offline(self, reason = None):
+        self.is_online = False
+        
+    def get_status(self):
+        return self.is_online, self.last_seen
+    
+#class Ping:
+#    def __init__(self, ip_addr):
+#        self.ip = ip_addr
+'''
+class Component:
+    def __init__(self, comp_name, comp_data = None):
+        self.name = comp_name
+        self.last_seen = time()
+        self.is_on = False
+        self.is_online = False
+        self.data = comp_data
+        print("Initializing component", comp_name)
+        wait()
+
+    def came_online(self):
+        if self.is_on == False:
+            self.is_on = True
+        self.is_online = True
+        self.last_seen = time()
+
+    def went_offline(self, reason=None):
+        self.is_online = False
+
+    def get_status(self):
+        return self.is_online, self.last_seen
+
+
+
+
+class System(Component):
     def __init__(self, sys_id):
         self.id = sys_id
-        self.online = self.ping()
-        print( "System Initialized. ID = ", sys_id )
+        self.debug = True
+        self.reg_id = None
+
+        if self.debug:
+            self.system = Component("debug_iface")
+        else:
+            self.system = Component("running_iface")
+
+        if self.ping():
+            self.system.came_online()
+        print( "System Initialized. ID = ", sys_id, "debugging =", self.debug )
 
         # unused
-        self.iface = "interface"
+        #self.iface = "interface"
 
     def ping(self):
-        return randint(0,1)
+        if self.debug:
+            wait_r()
+            return randint(0,1)
+        else:
+            return 1
 
     def register(self):
-        print("System registred on", self.iface)
+        if self.reg_id == None:
+            on = self.ping()
+            t = time()
+            self.reg_id = t
+            if on:
+                self.system.came_online()
 
-#class Network:
-#    def __init__(self, sys_ids):
+            print("System", self.system.name)
+            print("registered at time", t)
+            print("Status:",on)
 
-if __name__ == "__main__":ssss
+
+class Network:
+    def __init__(self, sys_ids):
+        self.networks = sys_ids
+        self.nws = [System(id) for id in sys_ids]
+
+    def report(self):
+        return list(map(s.ping() for s in self.nws))
+
+if __name__ == "__main__":
     a = System(1)
     b = System(5)
     print(a.ping(),b.ping())
+
+    n = Network([1,2,3,4,5,6,7,8,9])
+    n.report()
